2cced427a3c4dea17d9a33b49b8e75136af89c32
atelier-aline <jefeletrica@gmail.com>
2026-02-26 16:12:34 -0300

    Forçar atualização de cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'atelier-v1';
+const CACHE_NAME = 'atelier-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -11,13 +11,28 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
